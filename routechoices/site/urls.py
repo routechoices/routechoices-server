6fdd48b45b04afd8e29a2151547ba9b8c817b1df
Raphaël Stefanini <rphl@rphl.net>
2019-02-05 17:20:06 +0200

    Add TOS an Privacy Policy and links

--- routechoices/site/urls.py
+++ routechoices/site/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^events/?$', views.events_view, name='events_view'),
     url(r'^tracker/?$', views.tracker_view, name='tracker_view'),
     url(r'^privacy-policy/?$', TemplateView.as_view(template_name='site/privacy_policy.html'), name='privacy_policy_view'),
+    url(r'^tos/?$', TemplateView.as_view(template_name='site/tos.html'), name='tos_view'),
     url(r'^(?P<slug>[0-9a-zA-Z_-]+)/?$', views.club_view, name='club_view'),
     url(r'^(?P<club_slug>[0-9a-zA-Z_-]+)/(?P<slug>[0-9a-zA-Z_-]+)/?$', views.event_view, name='event_view'),
     url(r'^(?P<club_slug>[0-9a-zA-Z_-]+)/(?P<slug>[0-9a-zA-Z_-]+)/export/?$', views.event_export_view, name='event_export_view'),
