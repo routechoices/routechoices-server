08c2ad37c773b47c695701c80d35d4937c4466e3
Raphaël Stefanini <rphl@rphl.net>
2019-05-30 12:55:14 +0300

    Add tests for GPS upload api endpoints

--- routechoices/api/tests.py
+++ routechoices/api/tests.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
+from routechoices.core.models import Location
+
 
 class ApiTestCase(APITestCase):
     def setUp(self):
@@ -39,6 +41,35 @@ class ApiTestCase(APITestCase):
         self.assertTrue(len(res.data.get('device_id')) == 6)
         self.assertTrue(res.data.get('device_id') != self.get_device_id())
 
-    def test_traccar_gw_api(self):
-        pass
+    def test_traccar_api_gw(self):
+        url = self.reverse_and_check('api:traccar_api_gw', '/api/traccar')
+        dev_id = self.get_device_id()
+        t = time.time()
+        res = self.client.post(
+            '{}?id={}&lat={}&lon={}&timestamp={}'.format(
+                url,
+                dev_id,
+                30.21,
+                40.32,
+                t
+            ))
+        self.assertEquals(res.status_code, status.HTTP_200_OK)
+        nb_points = Location.objects.filter(device__aid=dev_id).count()
+        self.assertEquals(nb_points, 1)
 
+    def test_garmin_api_gw(self):
+        url = self.reverse_and_check('api:garmin_api_gw', '/api/garmin')
+        dev_id = self.get_device_id()
+        t = time.time()
+        res = self.client.post(
+            url,
+            {
+                'device_id': dev_id,
+                'latitudes': '1.1,1.2',
+                'longitudes': '3.1,3.2',
+                'timestamps': '{},{}'.format(t, t+1),
+            }
+        )
+        self.assertEquals(res.status_code, status.HTTP_200_OK)
+        nb_points = Location.objects.filter(device__aid=dev_id).count()
+        self.assertEquals(nb_points, 2)
