e9d0acd22199d05c9681dd6a29103f9479a5bca2
Raphaël Stefanini <rphl@rphl.net>
2020-04-16 15:32:03 +0300

    Rewrite purge_unused_locations command

--- routechoices/core/management/commands/purge_unused_locations.py
+++ routechoices/core/management/commands/purge_unused_locations.py
@@ -1,22 +1,9 @@
 from django.core.management.base import BaseCommand
-from routechoices.core.models import Location, Event
+from routechoices.core.models import Location, Event, Device
 
 from datetime import timedelta
 from django.db.backends.signals import connection_created
 from django.utils.timezone import now
-
-
-def set_timeout_on_new_conn(sender, connection, **kwargs):
-    """
-        Rig django to set statement timeout for each new connection based on the config
-    """
-    timeout_to_set = 300000     # Set timeout to 5 minutes
-
-    with connection.cursor() as cursor:
-        cursor.execute("set statement_timeout={0}".format(timeout_to_set))
-
-
-connection_created.connect(set_timeout_on_new_conn)
 
 
 class Command(BaseCommand):
@@ -27,29 +14,47 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         force = options['force']
-        qs = Location.objects.filter(
-            datetime__lt=now() - timedelta(days=14),
-        )
-        events = Event.objects.all()
-        for event in events:
-            for competitor in event.competitors.all():
-                if competitor.device:
-                    filter_args = {
-                        'device': competitor.device,
-                        'datetime__gte': max(
-                            competitor.start_time,
-                            event.start_date
-                        )
-                    }
-                    if event.end_date:
-                        filter_args['datetime__lte'] = event.end_date
-                    qs = qs.exclude(**filter_args)
+        deleted_count = 0
+        devices = Device.objects.all()
+        for device in devices:
+            periods_used = []
+            competitors = device.competitor_set.all()
+            for competitor in competitors:
+                event = competitor.event
+                start = event.start_date
+                if competitor.start_time:
+                    start = competitor.start_time
+                end = None
+                if event.end_date:
+                    end = min(event.end_date, now() - timedelta(days=14))
+                else:
+                    end = now() - timedelta(days=14)
+                periods_used.append((start, end))
+            locs = device.locations
+            valid_indexes = []
+            for idx, timestamp in enumerate(locs['timestamps']):
+                is_valid = False
+                for p in periods_used:
+                    if p[0].timestamp() < timestamp < p[1].timestamp():
+                        is_valid = True
+                        break
+                if is_valid:
+                    valid_indexes.append(idx)
+            device_deleted_loc_count = len(locs['timestamps']) - len(valid_indexes)
+            deleted_count += device_deleted_loc_count
+            if force and device_deleted_loc_count:
+                new_locs = {
+                    'timestamps': [locs['timestamps'][i] for i in valid_indexes],
+                    'latitudes': [locs['latitudes'][i] for i in valid_indexes],
+                    'longitudes': [locs['longitudes'][i] for i in valid_indexes],
+                }
+                device.locations = new_locs
+                device.save()
         if force:
-            deleted_count, _ = qs.delete()
             self.stdout.write(
                 self.style.SUCCESS(
                     'Successfully removed %d Locations' % deleted_count
                 )
             )
         else:
-            self.stdout.write('Would remove %d Locations' % qs.count())
+            self.stdout.write('Would remove %d Locations' % deleted_count)
