04c0176dea35603fad27ed87c3beaff71072268b
Raphaël Stefanini <raphael.stefanini@ecom.fi>
2021-01-26 16:40:04 +0200

    Admin interface improvements

--- routechoices/core/admin.py
+++ routechoices/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django.db.models import Count
+from django.db.models.functions import Length
 
 from routechoices.core.models import (
     Club,
@@ -13,10 +14,62 @@ from routechoices.core.models import (
 )
 
 
+class HasLocationFilter(admin.SimpleListFilter):
+    title = 'Weither It Has Locations'
+    parameter_name = 'has_locations'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('has_locations', 'With locations'),
+            ('has_no_locations', 'Without locations'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'has_no_locations':
+            return queryset.filter(
+                locations_raw_length=53
+            )
+        elif self.value():
+            return queryset.filter(
+                locations_raw_length__gt=53
+            )
+
+
+class HasCompetitorFilter(admin.SimpleListFilter):
+    title = 'Weither It Has Competitors'
+    parameter_name = 'has_competitors'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('has_competitors', 'With competitors'),
+            ('has_no_competitors', 'Without competitors'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'has_no_competitors':
+            return queryset.filter(
+                num_competitors=0
+            )
+        elif self.value():
+            return queryset.filter(
+                num_competitors__gt=0
+            )
+
+
 class ClubAdmin(admin.ModelAdmin):
     list_display = (
-        'name',
+        'name', 'num_events',
     )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+                num_events=Count('events')
+            )
+
+    def num_events(self, obj):
+        return obj.num_events
+
+    num_events.admin_order_field = 'num_events'
 
 
 class CompetitorInline(admin.TabularInline):
@@ -76,11 +129,14 @@ class DeviceAdmin(admin.ModelAdmin):
     actions = ['clean_positions']
     search_fields = ('aid', )
     inlines = [DeviceCompetitorInline, ]
+    list_filter = (HasCompetitorFilter, HasLocationFilter, )
 
     def get_queryset(self, request):
-        return super().get_queryset(request).extra(
-            select={'locations_raw_length': 'Length(locations_raw)'}
-        ).annotate(num_competitors=Count('competitor_set'))
+        return super().get_queryset(request).annotate(
+                locations_raw_length=Length('locations_raw')
+            ).annotate(
+                num_competitors=Count('competitor_set')
+            )
 
     def location_count(self, obj):
         return obj.location_count
