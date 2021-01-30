6375ed40c9bdb68171bed78a9834b3093c0f1a88
Raphaël Stefanini <raphael.stefanini@ecom.fi>
2021-01-30 10:34:31 +0200

    More cy tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,54 +1,58 @@
 context('Cypress tests', () => {
   beforeEach(() => {
     // https://on.cypress.io/visit
+    cy.exec('docker exec dz01 /venv/bin/python3 /app/manage.py reset_db_for_tests')
     cy.visit('/')
   })
 
   it('Create an Event', function () {
-    cy.login()
-    cy.url().should('include', '/dashboard/')
+    cy.getDeviceId().then(devId => {
+      cy.login()
+      cy.url().should('match', /\/dashboard\/$/)
 
-    // Create club
-    cy.visit('/dashboard')
-    cy.get('a').contains('Clubs').click()
-    cy.url().should('include', '/dashboard/club')
-    cy.get('a').contains('Create new club').click()
-    cy.url().should('include', '/dashboard/club/new')
-    cy.get('#id_name').type('Halden SK')
-    cy.get('#id_slug').type('halden-sk')
-    cy.get('#id_admins').next('').type('admin{enter}')
-    cy.get("input[value='Submit']").click()
-    cy.url().should('include', '/dashboard/club')
 
-    // Create Map
-    cy.visit('/dashboard')
-    cy.get('a').contains('Maps').click()
-    cy.url().should('include', '/dashboard/map')
-    cy.get('a').contains('Create new map').click()
-    cy.url().should('include', '/dashboard/map/new')
-    
-    cy.get('#id_club').select('Halden SK')
-    cy.get('#id_name').type('Jukola 2019 - 1st Leg')
-    const mapFileName = 'Jukola_1st_leg_blank_61.45075_24.18994_61.44656_24.24721_61.42094_24.23851_61.42533_24.18156_.jpg'
-    cy.get('#id_image').attachFile(mapFileName)
-    cy.get("input[value='Submit']").click()
-    cy.url().should('include', '/dashboard/map')
+      // Create club
+      cy.createClub()
 
-    // Create Event
-    cy.visit('/dashboard/event')
-    cy.url().should('include', '/dashboard/event')
-    cy.get('a').contains('Create new event').click()
-    cy.url().should('include', '/dashboard/event/new')
+      // Create Map
+      cy.createMap()
 
-    cy.get('#id_club').select('Halden SK')
-    cy.get('#id_name').type('Jukola 2019 - 1st Leg')
-    cy.get('#id_slug').type('Jukola-2019-1st-leg')
-    cy.get('#id_start_date').type('2019-06-15 20:00:00{enter}')
-    cy.get('#id_end_date').type('2019-06-16 00:00:00{enter}')
-    cy.get('#id_map').select('Jukola 2019 - 1st Leg (Halden SK)')
+      // Create Event with minimal info
+      cy.visit('/dashboard/event')
+      cy.url().should('match', /\/dashboard\/event$/)
+      cy.get('a').contains('Create new event').click()
+      cy.url().should('match', /\/dashboard\/event\/new$/)
 
-    cy.get("input[value='Submit']").click()
-    cy.url().should('include', '/dashboard/event')
-    cy.visit('/halden-sk/Jukola-2019-1st-leg')
+      cy.get('#id_club').select('Halden SK')
+      cy.get('#id_name').type('Jukola 2019 - 1st Leg')
+      cy.get('#id_slug').type('Jukola-2019-1st-leg')
+      cy.get('#id_start_date').type('2019-06-15 20:00:00{enter}')
+      cy.get('#id_map').select('Jukola 2019 - 1st Leg (Halden SK)')
+
+      cy.get("input[value='Submit']").click()
+      cy.url().should('match', /\/dashboard\/event$/)
+      cy.visit('/halden-sk/Jukola-2019-1st-leg')
+
+      // Create Event with all fields info
+      cy.visit('/dashboard/event')
+      cy.url().should('match', /\/dashboard\/event$/)
+      cy.get('a').contains('Create new event').click()
+      cy.url().should('match', /\/dashboard\/event\/new$/)
+
+      cy.get('#id_club').select('Halden SK')
+      cy.get('#id_name').type('Jukola 2019 - 1st Leg (2)')
+      cy.get('#id_slug').type('Jukola-2019-1st-leg-2')
+      cy.get('#id_start_date').type('2019-06-15 20:00:00{enter}')
+      cy.get('#id_end_date').type('2019-06-16 00:00:00{enter}')
+      cy.get('#id_map').select('Jukola 2019 - 1st Leg (Halden SK)')
+      cy.get('#id_competitors-0-device-selectized').type(devId)
+      cy.get('#id_competitors-0-name').type('Mats Haldin')
+      cy.get('#id_competitors-0-short_name').type('Halden')
+      cy.get('#id_competitors-0-start_time').type('2019-06-15 20:00:10{enter}')
+      
+      cy.get("input[value='Submit']").click()
+      cy.url().should('match', /\/dashboard\/event$/)
+      cy.visit('/halden-sk/Jukola-2019-1st-leg-2')
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,9 +26,49 @@
 import 'cypress-file-upload';
 
 Cypress.Commands.add("login", (username='admin', password='pa$$word123') => { 
-    cy.visit('/accounts/login/')
-    cy.get('#id_login')
-      .type(username)
-    cy.get('#id_password')
-      .type(password + '{enter}')
+  cy.visit('/accounts/login/')
+  cy.get('#id_login').type(username)
+  cy.get('#id_password').type(password + '{enter}')
+ })
+
+ Cypress.Commands.add("createClub", () => {
+  cy.visit('/dashboard/')
+  cy.get('a').contains('Clubs').click()
+  cy.url().should('match', /\/dashboard\/club$/)
+  cy.get('a').contains('Create new club').click()
+  cy.url().should('match', /\/dashboard\/club\/new$/)
+  cy.get('#id_name').type('Halden SK')
+  cy.get('#id_slug').type('halden-sk')
+  cy.get('#id_admins').next('').type('admin{enter}')
+  cy.get("input[value='Submit']").click()
+  cy.url().should('match', /\/dashboard\/club$/)
+ })
+
+ Cypress.Commands.add("getDeviceId", () => {
+  cy.clearCookies()
+  return cy.request({
+    method: 'POST',
+    url: '/api/device_id/',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      cookie: ''
+    },
+  }).then(response => {
+    return cy.wrap(response.body.device_id)
+  })
+ })
+
+ Cypress.Commands.add("createMap", () => {
+  cy.visit('/dashboard/')
+  cy.get('a').contains('Maps').click()
+  cy.url().should('match', /\/dashboard\/map$/)
+  cy.get('a').contains('Create new map').click()
+  cy.url().should('match', /\/dashboard\/map\/new$/)
+  
+  cy.get('#id_club').select('Halden SK')
+  cy.get('#id_name').type('Jukola 2019 - 1st Leg')
+  const mapFileName = 'Jukola_1st_leg_blank_61.45075_24.18994_61.44656_24.24721_61.42094_24.23851_61.42533_24.18156_.jpg'
+  cy.get('#id_image').attachFile(mapFileName)
+  cy.get("input[value='Submit']").click()
+  cy.url().should('match', /\/dashboard\/map$/)
  })
