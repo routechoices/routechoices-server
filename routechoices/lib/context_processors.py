66f186818a15cb9793e21734d16ede08acfda450
Raphaël Stefanini <raphael@rphlo.com>
2021-07-28 11:01:07 +0300

    Use panel bear only in prod

--- routechoices/lib/context_processors.py
+++ routechoices/lib/context_processors.py
@@ -1,7 +1,8 @@
 from django.contrib.sites.models import Site
-
+from django.conf import settings
 
 def site(request):
     return {
-        'site': Site.objects.get_current()
+        'site': Site.objects.get_current(),
+        'panelbear_id': getattr(settings, 'PANELBEAR_ID', None)
     }
