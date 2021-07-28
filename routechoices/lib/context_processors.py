00b9180bac76f7d965ecd435cf87258c67fdea78
Raphaël Stefanini <raphael@rphlo.com>
2021-07-28 11:12:33 +0300

    Add verion to sentry

--- routechoices/lib/context_processors.py
+++ routechoices/lib/context_processors.py
@@ -1,8 +1,17 @@
 from django.contrib.sites.models import Site
 from django.conf import settings
+import subprocess
+
+def get_git_revision_hash():
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf8').strip()
+
+def get_git_revision_short_hash():
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('utf8').strip()
+
 
 def site(request):
     return {
         'site': Site.objects.get_current(),
-        'panelbear_id': getattr(settings, 'PANELBEAR_ID', None)
+        'panelbear_id': getattr(settings, 'PANELBEAR_ID', None),
+        'version': get_git_revision_hash(),
     }
