81d8cde82b75619bee870b6f2e06e0be1b05ae2e
Raphaël Stefanini <raphael@rphlo.com>
2021-07-30 14:34:01 +0300

    Set directory to execute command to avoid possible errors

--- routechoices/lib/context_processors.py
+++ routechoices/lib/context_processors.py
@@ -3,10 +3,10 @@ from django.conf import settings
 import subprocess
 
 def get_git_revision_hash():
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf8').strip()
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=settings.BASE_DIR).decode('utf8').strip()
 
 def get_git_revision_short_hash():
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('utf8').strip()
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=settings.BASE_DIR).decode('utf8').strip()
 
 
 def site(request):
