6f3b989c48181306c1dd7d9015b10b30cd7fad51
Raphaël Stefanini <raphael@rphlo.com>
2021-08-14 17:11:40 +0300

    Pause WMS service while it doesnt work properly

--- static_assets/js/event-2021081402.js
+++ static_assets/js/event-2021081402.js
@@ -860,11 +860,10 @@ function addRasterMap(a, b, c, d, src, fit) {
     fit = false;
   }
   var bounds = [a, b, c, d];
-  rasterMap = L.tileLayer.wms(wmsService+'?', {
-      layers: eventId,
-      bounds: bounds,
-      tileSize: 512,
-  }).addTo(map);
+  new L.ImageTransform(
+    src,
+    bounds
+  ).addTo(map);
   if(fit) {
     var z = map.fitBounds(bounds);
   }
