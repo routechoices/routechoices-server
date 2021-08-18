2c9881dca293663eb9c067b7b9619558513bb8cd
Raphaël Stefanini <raphael@rphlo.com>
2021-08-18 14:09:05 +0300

    Add certification creation and renewal commands

---
+++ routechoices/core/management/commands/create_certificate.py
@@ -0,0 +1,119 @@
+import os.path
+import sys
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+import sewer.client
+from sewer.crypto import AcmeKey, AcmeAccount
+from sewer.auth import ProviderBase
+
+from routechoices.core.models import Club
+from typing import cast
+
+
+def write_nginf_conf(domain):
+    with open(os.path.join(settings.BASE_DIR, 'nginx', 'custom_domains', f'{domain}'), 'w') as f:
+        f.write(f'''server {{
+    server_name {domain};
+    if ($host = {domain}) {{
+        return 302 https://$host$request_uri;
+    }}
+    listen 80;
+    listen [::]:80;
+    return 404;
+}}
+
+server {{
+    server_name {domain};
+
+    ssl_certificate {os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.crt')};
+    ssl_certificate_key {os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.key')};
+    listen 443;
+    listen [::]:443;
+
+    location / {{
+       client_max_body_size    10M;
+       proxy_set_header Host   $host;
+       proxy_set_header        X-Real-IP $remote_addr;
+       proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
+       uwsgi_pass              unix://{os.path.join(settings.BASE_DIR, 'var', 'django.sock')};
+       uwsgi_pass_header       Authorization;
+       uwsgi_hide_header       X-Accel-Redirect;
+       uwsgi_hide_header       X-Sendfile;
+       uwsgi_intercept_errors  off;
+       include                 uwsgi_params;
+    }}
+}}''')
+
+
+def write_account_key(self, filename):
+    with open(filename, "wb") as f:
+        if self.__kid:
+            f.write(("KID: %s\n" % self.__kid).encode())
+            if self._timestamp:
+                f.write(("Timestamp: %s\n" % self._timestamp).encode())
+        f.write(self.to_pem())
+
+
+class ClubProvider(ProviderBase):
+    def __init__(self, club, **kwargs):
+        kwargs["chal_types"] = ["http-01"]
+        self.club = club
+        super().__init__(**kwargs)
+        self.chal_type = "http-01"
+
+    def setup(self, challenges):
+        self.club.acme_challenge = challenges[0]['key_auth']
+        self.club.save()
+        return []
+    
+    def unpropagated(self, challenges):
+        # could add confirmation here, but it's just a demo
+        return []
+
+    def clear(self, challenges):
+        self.club.acme_challenge = ''
+        self.club.save()
+        return []
+
+
+
+class Command(BaseCommand):
+    help = 'Create letsencrypt certificate for club custom domain'
+
+    def add_arguments(self, parser):
+        parser.add_argument('domains', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        nginx_need_restart = False
+        for domain in options['domains']:
+            club = Club.objects.filter(domain=domain).first()
+            if not club:
+                self.stderr.write('No club with this domain')
+                continue
+        
+            if os.path.exists(os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.key')):
+                self.stderr.write('Certificates for this domain already exists')
+                continue
+        
+            client = sewer.client.Client(
+                domain_name=domain,
+                provider=ClubProvider(club),
+                account= AcmeAccount.create("secp256r1"),
+                cert_key=AcmeKey.create("secp256r1"),
+                is_new_acct=True,
+                contact_email='raphael@routechoices.com'
+            )
+            certificate = client.get_certificate()
+            cert_key = client.cert_key
+            acct_key = client.account
+
+            with open(os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.crt'), 'w') as f:
+                f.write(certificate)
+            cert_key.write_pem(os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.key'))
+            write_account_key(acct_key, os.path.join(settings.BASE_DIR, 'nginx', 'certs', 'accounts', f'{domain}.key'))
+            write_nginf_conf(domain)
+        if nginx_need_restart:
+            print('Reload nginx for changes to take effect...')
+        
+
---
+++ routechoices/core/management/commands/renew_certificate.py
@@ -0,0 +1,140 @@
+import os
+import os.path
+import subprocess
+import sys
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+import sewer.client
+from sewer.crypto import AcmeKey, AcmeAccount
+from sewer.auth import ProviderBase
+
+from routechoices.core.models import Club
+from typing import cast
+
+
+def is_expirying(domain):
+    r = subprocess.run(
+        ['openssl', 'x509', '-checkend', '450000', '-noout', '-in', f'{domain}.crt'],
+        cwd=os.path.join(settings.BASE_DIR, 'nginx', 'certs'),
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True
+    )
+    return r.stdout.startswith('Certificate will expire')
+
+
+def read_account_key(cls, filename: str):
+        with open(filename, "rb") as f:
+            data = f.read()
+        prefix = b""
+        n = data.find(b"-----BEGIN")
+        if 0 < n:
+            prefix = data[:n]
+            data = data[n:]
+        acct = cast("AcmeAccount", cls.from_pem(data))
+        if prefix:
+            parts = prefix.split(b"\n")
+            for p in parts:
+                if p.startswith(b"KID: "):
+                    acct.__kid = p[5:].decode()
+                elif p.startswith(b"Timestamp: "):
+                    acct._timestamp = float(p[11:])
+        return acct
+
+
+def write_account_key(self, filename):
+    with open(filename, "wb") as f:
+        if self.__kid:
+            f.write(("KID: %s\n" % self.__kid).encode())
+            if self._timestamp:
+                f.write(("Timestamp: %s\n" % self._timestamp).encode())
+        f.write(self.to_pem())
+
+class ClubProvider(ProviderBase):
+    def __init__(self, club, **kwargs):
+        kwargs["chal_types"] = ["http-01"]
+        self.club = club
+        super().__init__(**kwargs)
+        self.chal_type = "http-01"
+
+    def setup(self, challenges):
+        self.club.acme_challenge = challenges[0]['key_auth']
+        self.club.save()
+        return []
+    
+    def unpropagated(self, challenges):
+        # could add confirmation here, but it's just a demo
+        return []
+
+    def clear(self, challenges):
+        self.club.acme_challenge = ''
+        self.club.save()
+        return []
+
+
+class Command(BaseCommand):
+    help = 'Renew letsencrypt certificate for club custom domain'
+
+    def add_arguments(self, parser):
+        parser.add_argument('domains', nargs='*', type=str)
+        parser.add_argument('--post-hook', dest='post-hook', type=str, default=None)
+
+    def handle(self, *args, **options):
+        nginx_need_restart = False
+
+        domains = options['domains']
+
+        if not domains:
+            domains = Club.objects.exclude(domain='').values_list('domain', flat=True)
+
+        for domain in domains:
+            self.stdout.write(f'Processing {domain} ...')
+            club = Club.objects.filter(domain=domain).first()
+            if not club:
+                self.stderr.write('No club with this domain')
+                continue
+
+            if not os.path.exists(os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.key')):
+                self.stderr.write('No certificates found for this domain found')
+                continue
+
+            if not is_expirying(domain):
+                self.stderr.write('Domain is not yet expiring')
+                continue
+            
+            account_exists = os.path.exists(os.path.join(settings.BASE_DIR, 'nginx', 'certs', 'accounts', f'{domain}.key'))
+            if account_exists:
+                acct_key = read_account_key(AcmeAccount, os.path.join(settings.BASE_DIR, 'nginx', 'certs', 'accounts', f'{domain}.key'))
+            else:
+                acct_key = AcmeAccount.create("secp256r1")
+                write_account_key(acct_key, os.path.join(settings.BASE_DIR, 'nginx', 'certs', 'accounts', f'{domain}.key'))
+
+            cert_key = AcmeKey.from_file(os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.key'))
+
+            client = sewer.client.Client(
+                domain_name=domain,
+                provider=ClubProvider(club),
+                account=acct_key,
+                cert_key=cert_key,
+                is_new_acct=(not account_exists),
+                contact_email='raphael@routechoices.com'
+            )
+            certificate = client.get_certificate()
+            cert_key = client.cert_key
+            acct_key = client.account
+
+            with open(os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.crt'), 'w') as f:
+                f.write(certificate)
+            cert_key.write_pem(os.path.join(settings.BASE_DIR, 'nginx', 'certs', f'{domain}.key'))
+            nginx_need_restart = True
+            self.stdout.write(f'Certificate renewed.')
+        if nginx_need_restart:
+            print('Reload nginx for changes to take effect...')
+            if options['post-hook']:
+                subprocess.run(
+                    options['post-hook'], shell=True, stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE, universal_newlines=True,
+                    check=False
+                )
