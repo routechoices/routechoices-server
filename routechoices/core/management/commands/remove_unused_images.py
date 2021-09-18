7df9cb4384107457edb0bce8ba1b5ce22b695f08
Raphaël Stefanini <raphael@rphlo.com>
2021-09-18 10:44:34 +0300

    Add script to clean storage [skip ci]

---
+++ routechoices/core/management/commands/remove_unused_images.py
@@ -0,0 +1,58 @@
+from django.core.management.base import BaseCommand
+
+from routechoices.core.models import Map
+from routechoices.lib.s3 import get_s3_client, s3_delete_key
+
+
+class Command(BaseCommand):
+    help = 'Remove old images files from db'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--dry-run', action='store_true', default=False)
+
+    def scan_map_directory(self):
+        
+        # Should use v2 but wasabi fails to list all files with it
+        # paginator = s3.get_paginator('list_objects_v2')
+        paginator = self.s3.get_paginator('list_objects')
+        kwargs = {
+          'Bucket': 'routechoices-maps',
+          'Prefix': 'maps',
+        }
+        for page in paginator.paginate(**kwargs):
+            try:
+                contents = page["Contents"]
+            except KeyError:
+                break
+            for obj in contents:
+                key = obj['Key']
+                yield key
+
+    def process_image_file(self, image_name, dry_run):
+        if image_name not in self.maps_image_paths:
+            self.n_image_removed += 1
+            self.stdout.write(f'File {image_name} is unused')
+            if not dry_run:
+                s3_delete_key(image_name, 'routechoices-maps')
+        else:
+            self.n_image_keeped += 1
+            self.keeped.add(image_name)
+
+    def handle(self, *args, **options):
+        dry_run = options['dry_run']
+        self.maps_image_paths = set(Map.objects.all().values_list('image', flat=True))
+        self.n_image_removed = 0
+        self.keeped = set()
+        self.n_image_keeped = 0
+        self.s3 = get_s3_client()
+        for filename in self.scan_map_directory():
+            self.process_image_file(filename, dry_run)
+
+        if not dry_run:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    'Successfully removed %d files, keeping %d' % (self.n_image_removed, self.n_image_keeped)
+                )
+            )
+        else:
+            self.stdout.write('Would remove %d files, keeping %d' % (self.n_image_removed, self.n_image_keeped))
