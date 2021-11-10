062a7c4c8521b9913d7009ce67b0ab292a510416
Raphaël Stefanini <raphael@rphlo.com>
2021-11-10 19:29:52 +0200

    JS cleanup

--- docker/django.dockerfile
+++ docker/django.dockerfile
@@ -1,4 +1,4 @@
-FROM python:3-slim
+FROM python:3.9-slim
 
 # Copy in your requirements file
 ADD requirements.txt /requirements.txt
--- static_assets/scripts/club/event.js
+++ static_assets/scripts/club/event.js
@@ -1,5 +1,5 @@
 if (!navigator.canShare) {
-  $('#share_buttons').hide();
+  $('#share_buttons').hide()
 }
 
 function checkImageFormatCapability(format) {
@@ -7,112 +7,109 @@ function checkImageFormatCapability(format) {
     var kTestImages = {
         webp: "data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
         avif: 'data:image/avif;base64,AAAAFGZ0eXBhdmlmAAAAAG1pZjEAAACgbWV0YQAAAAAAAAAOcGl0bQAAAAAAAQAAAB5pbG9jAAAAAEQAAAEAAQAAAAEAAAC8AAAAGwAAACNpaW5mAAAAAAABAAAAFWluZmUCAAAAAAEAAGF2MDEAAAAARWlwcnAAAAAoaXBjbwAAABRpc3BlAAAAAAAAAAQAAAAEAAAADGF2MUOBAAAAAAAAFWlwbWEAAAAAAAAAAQABAgECAAAAI21kYXQSAAoIP8R8hAQ0BUAyDWeeUy0JG+QAACANEkA='
-    };
-    var img = new Image();
+    }
+    var img = new Image()
     img.onload = function () {
-        var result = (img.width > 0) && (img.height > 0);
-        res(result);
-    };
+        var result = (img.width > 0) && (img.height > 0)
+        res(result)
+    }
     img.onerror = function () {
-        res(false);
-    };
-    img.src = kTestImages[format];
-  });
+        res(false)
+    }
+    img.src = kTestImages[format]
+  })
 }
 
-var hasWebpSupport = false;
-var hasAvifSupport = false;
+var hasWebpSupport = false
+var hasAvifSupport = false
 
-(function (){
+;(function (){
   checkImageFormatCapability('webp').then(function(res){hasWebpSupport = res})
   checkImageFormatCapability('avif').then(function(res){hasAvifSupport = res})
 })()
 
 function shareUrl (e) {
-  e.preventDefault();
+  e.preventDefault()
   var shareData = {
     title: $('meta[property="og:title"]').attr('content'),
     text: $('meta[property="og:description"]').attr('content'),
     url: window.location
   }
   try {
-    navigator.share(shareData).then(function () {}).catch(function () {});
+    navigator.share(shareData).then(function () {}).catch(function () {})
   } catch(err) {
   }
-};
+}
 
 $(function() {
-  $('.page-alerts').hide();
+  $('.page-alerts').hide()
   $('.page-alert .close').on('click', function(e) {
-    e.preventDefault();
-    $(this).closest('.page-alert').slideUp();
-  });
+    e.preventDefault()
+    $(this).closest('.page-alert').slideUp()
+  })
 
   $('#runners_show_button').on('click', function(e){
-    e.preventDefault();
+    e.preventDefault()
     if($('#sidebar').hasClass('d-none')){
-      $('#sidebar').removeClass('d-none').addClass('col-12');
-      $('#map').addClass('d-none').removeClass('col-12');
+      $('#sidebar').removeClass('d-none').addClass('col-12')
+      $('#map').addClass('d-none').removeClass('col-12')
     }else if(!chatDisplayed){
-      $('#sidebar').addClass('d-none').removeClass('col-12');
-      $('#map').removeClass('d-none').addClass('col-12');
+      $('#sidebar').addClass('d-none').removeClass('col-12')
+      $('#map').removeClass('d-none').addClass('col-12')
       map.invalidateSize()
     }
     displayCompetitorList(true)
   })
 
-  $('#live_button').on('click', selectLiveMode);
-  $('#replay_button').on('click', selectReplayMode);
-  $('#play_pause_button').on('click', pressPlayPauseButton);
+  $('#live_button').on('click', selectLiveMode)
+  $('#replay_button').on('click', selectReplayMode)
+  $('#play_pause_button').on('click', pressPlayPauseButton)
   $('#next_button').on('click', function(e){
-    e.preventDefault();
-    playbackRate = playbackRate*2;
-  });
+    e.preventDefault()
+    playbackRate = playbackRate * 2
+  })
   $('#prev_button').on('click', function(e){
-    e.preventDefault();
-    playbackRate = Math.max(1, playbackRate/2);
-  });
-  $('#real_time_button').on('click', function(e){
-    e.preventDefault();
-    isRealTime=true;
+    e.preventDefault()
+    playbackRate = Math.max(1, playbackRate/2)
+  })
+  $('#real_time_button').on('click', function(e) {
+    e.preventDefault()
+    isRealTime = true
     if (resetMassStartContextMenuItem) {
-      map.contextmenu.removeItem(resetMassStartContextMenuItem);
-      resetMassStartContextMenuItem = null;
+      map.contextmenu.removeItem(resetMassStartContextMenuItem)
+      resetMassStartContextMenuItem = null
     }
-    $('#real_time_button').addClass('active');
-    $('#mass_start_button').removeClass('active');
-  });
-  $('#mass_start_button').on('click', function(e){
-    e.preventDefault();
+    $('#real_time_button').addClass('active')
+    $('#mass_start_button').removeClass('active')
+  })
+  $('#mass_start_button').on('click', function(e) {
+    e.preventDefault()
     onPressResetMassStart()
-  });
+  })
 
   $('#chat_show_button').on('click', displayChat)
   $('#options_show_button').on('click', displayOptions)
   $('#full_progress_bar').on('click', pressProgressBar)
   $('#share_button').on('click', shareUrl)
 
-  var thumb = document.querySelector('#full_progress_bar');
+  var thumb = document.querySelector('#full_progress_bar')
   thumb.onmousedown = function(event) {
-      event.preventDefault(); // prevent selection start (browser action)
-
-      document.addEventListener('mousemove', pressProgressBar);
-      document.addEventListener('mouseup', onMouseUp);
-
+      event.preventDefault()
+      document.addEventListener('mousemove', pressProgressBar)
       function onMouseUp() {
-        document.removeEventListener('mouseup', onMouseUp);
-        document.removeEventListener('mousemove', pressProgressBar);
+        document.removeEventListener('mouseup', onMouseUp)
+        document.removeEventListener('mousemove', pressProgressBar)
       }
-
-  };
+      document.addEventListener('mouseup', onMouseUp)
+  }
   thumb.ondragstart = function() {
-      return false;
-  };
+      return false
+  }
 
   $('.date-utc').each(function(i, el){
-    $el = $(el);
-    $el.text(dayjs($el.data('date')).local().format('MMMM D, YYYY [at] HH:mm:ss'));
-  });
+    $el = $(el)
+    $el.text(dayjs($el.data('date')).local().format('MMMM D, YYYY [at] HH:mm:ss'))
+  })
 
   map = L.map('map', {
     center: [15, 0],
@@ -140,7 +137,7 @@ $(function() {
         callback: zoomOut
       }
     ]
-  });
+  })
   panControl = L.control.pan()
   zoomControl = L.control.zoom()
   rotateControl = L.control.rotate({closeOnZeroBearing: false})
@@ -149,7 +146,7 @@ $(function() {
   zoomControl.addTo(map)
   rotateControl.addTo(map)
 
-  map.doubleClickZoom.disable(); 
+  map.doubleClickZoom.disable()
   map.on('dblclick', onPressCustomMassStart)
   map.on('move', function () { drawCompetitors() })
 
@@ -160,7 +157,7 @@ $(function() {
    },
    crossDomain: true,
   }).done(function(response){
-    backdropMaps[response.event.backdrop].addTo(map);
+    backdropMaps[response.event.backdrop].addTo(map)
     var now = new Date()
     var startEvent = new Date(response.event.start_date)
     var endEvent = new Date(response.event.end_date)
@@ -168,19 +165,19 @@ $(function() {
       $('#eventLoadingModal').remove()
       var preRaceModal = new bootstrap.Modal(document.getElementById("eventNotStartedModal"), {backdrop:'static', keyboard: false, })
       document.getElementById("eventNotStartedModal").addEventListener('hide.bs.modal', function(e){
-          e.preventDefault();
+          e.preventDefault()
       })
-      preRaceModal.show();
+      preRaceModal.show()
       window.setInterval(function(){
         if(new Date() > startEvent) {
-          location.reload();
+          location.reload()
         }
-      }, 1e3);
+      }, 1e3)
     } else {
       if (endEvent > now) {
         isLiveEvent = true
         if (response.event.chat_enabled) {
-          $('#chat_button_group').removeClass('d-none');
+          $('#chat_button_group').removeClass('d-none')
           $.ajax({
             url: 'https:'+ chatMessagesEndpoint,
             dataType: 'JSON'
@@ -190,15 +187,15 @@ $(function() {
           connectChat()
         }
       }
-      qrUrl = response.event.shortcut;
+      qrUrl = response.event.shortcut
       liveUrl = response.data
 
       if(response.maps.length) {
-        var MapLayers = {};
+        var MapLayers = {}
         for (var i=0; i < response.maps.length; i++) {
-          var m = response.maps[i];
+          var m = response.maps[i]
           if (m.default) {
-            m.title = m.title ? $('<span/>').text(m.title).html() : '<i class="fa fa-star"></i> Main Map';
+            m.title = m.title ? $('<span/>').text(m.title).html() : '<i class="fa fa-star"></i> Main Map'
             mapHash = m.hash
             mapUrl = m.url + '?map_hash=' + mapHash
             var bounds = [
@@ -230,7 +227,7 @@ $(function() {
           }
         }
         if (response.maps.length > 1) {
-            L.control.layers(MapLayers, null, {collapsed: false}).addTo(map);
+            L.control.layers(MapLayers, null, {collapsed: false}).addTo(map)
             map.on('baselayerchange', function(e) {
                 console.log(e)
                 map.fitBounds(e.layer.options.bounds)
@@ -240,22 +237,22 @@ $(function() {
         zoomOnRunners = true
       }
       if (response.announcement) {
-        prevNotice = response.announcement;
-        $('#alert-text').text(prevNotice);
-        $('.page-alert').slideUp(0);
-        $('.page-alerts').show();
-        $('.page-alert').slideDown();
+        prevNotice = response.announcement
+        $('#alert-text').text(prevNotice)
+        $('.page-alert').slideUp(0)
+        $('.page-alerts').show()
+        $('.page-alert').slideDown()
       }
       if (!setFinishLineContextMenuItem) {
         setFinishLineContextMenuItem = map.contextmenu.insertItem({
           text: 'Draw finish line',
           callback: drawFinishLine
-        }, 1);
+        }, 1)
       }
-      onStart();
+      onStart()
     }
   }).fail(function(){
     $('#eventLoadingModal').remove()
     swal({text: 'Something went wrong', title: 'error', type: 'error'})
-  });
+  })
 })
