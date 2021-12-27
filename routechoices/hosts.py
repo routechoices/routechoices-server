6a4193f949536092f6d3338e814ce65c5dbee4d0
Raphaël Stefanini <raphael@rphlo.com>
2021-12-27 15:37:49 +0200

    CRSF origi

--- routechoices/hosts.py
+++ routechoices/hosts.py
@@ -3,10 +3,10 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
     '',
-    host(r'^www$', 'routechoices.urls', name='www'),
-    host(r'^api$', 'routechoices.api.urls', name='api'),
+    host('www', 'routechoices.urls', name='www'),
+    host('api', 'routechoices.api.urls', name='api'),
     host(
-        r'^(?P<club_slug>[a-zA-Z0-9][a-zA-Z0-9-]+)$', 
+        '(?P<club_slug>[a-zA-Z0-9][a-zA-Z0-9-]+)', 
         'routechoices.club.urls',
         callback='routechoices.club.urls.set_club',
         name='clubs'),
