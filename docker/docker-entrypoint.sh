0133f90b24a7d4f4c608437c5a6eb682e4790dcd
Raphaël Stefanini <raphael@rphlo.com>
2021-12-31 12:41:56 +0200

    Update django entry point script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-/venv/bin/pip install -r pip.lock
 
 until psql $DATABASE_URL -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
@@ -11,6 +10,5 @@ done
 
 /venv/bin/python manage.py migrate --noinput
 /venv/bin/python manage.py collectstatic --noinput
-/venv/bin/python manage.py createcachetable --noinput
 
 exec "$@"
