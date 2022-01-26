f6af2e07966eaf88d33fa61ec2b1c8f3755dfa51
Raphaël Stefanini <raphael@rphlo.com>
2022-01-26 18:36:38 +0200

    Fix compressor settings

--- routechoices/settings.py
+++ routechoices/settings.py
@@ -175,7 +175,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_assets"),
 ]
-STATICFILES_FINDERS = ("compressor.finders.CompressorFinder",)
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
