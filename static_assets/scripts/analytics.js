27b93b6c38e26ad0552f2914671be765a1626020
Raphael Stefanini <raphael@rphlo.com>
2022-02-05 18:08:13 +0200

    Catch error if sentry is blocked

--- static_assets/scripts/analytics.js
+++ static_assets/scripts/analytics.js
@@ -1,9 +1,13 @@
 
-Sentry.init({
-    dsn: "https://90a1a7dd37134928b5a981eeb3a20293@o91052.ingest.sentry.io/198396",
-    release: "master@{{version}}",
-    integrations: [],
-});
+try{
+    Sentry.init({
+        dsn: "https://90a1a7dd37134928b5a981eeb3a20293@o91052.ingest.sentry.io/198396",
+        release: "master@{{version}}",
+        integrations: [],
+    });
+} catch(e) {
+    // Sentry was blocked
+}
 
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
