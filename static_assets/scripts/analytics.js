2a7f9fafa5605c1fd9945fb22e54a898eb7b352a
Raphaël Stefanini <raphael@rphlo.com>
2022-03-16 12:39:41 +0200

    Udpate browser sentry

--- static_assets/scripts/analytics.js
+++ static_assets/scripts/analytics.js
@@ -1,13 +1,3 @@
-try {
-  Sentry.init({
-    dsn: "https://90a1a7dd37134928b5a981eeb3a20293@o91052.ingest.sentry.io/198396",
-    release: "master@{{version}}",
-    integrations: [],
-  });
-} catch (e) {
-  // Sentry was blocked
-}
-
 var _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
