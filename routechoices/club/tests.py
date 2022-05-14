5702636b098e78e1ae3d8559627b01fda90a366b
Raphael Stefanini <raphael@rphlo.com>
2022-05-14 15:30:27 +0300

    Add more tests

---
+++ routechoices/club/tests.py
@@ -0,0 +1,93 @@
+import arrow
+from django.contrib.auth.models import User
+from django.test import Client, LiveServerTestCase
+
+from routechoices.core.models import Club, Event
+
+
+class ClubViewsTestCase(LiveServerTestCase):
+    def setUp(self):
+        self.client = Client(HTTP_HOST="kiilat.routechoices.dev:8081")
+        self.user = User.objects.create_user(
+            "alice", "alice@example.com", "pa$$word123"
+        )
+        self.club = Club.objects.create(name="Kemiön Kiilat", slug="kiilat")
+        self.club.admins.set([self.user])
+
+    def test_event_pages_loads(self):
+        e = Event.objects.create(
+            name="Kiila Cup 1",
+            slug="kiila-cup-1",
+            club=self.club,
+            start_date=arrow.now().shift(hours=-1).datetime,
+            end_date=arrow.now().shift(hours=1).datetime,
+        )
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-1")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-1/export")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-1/registration")
+        self.assertEqual(response.status_code, 404)
+        e.open_registration = True
+        e.save()
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-1/registration")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-1/route-upload")
+        self.assertEqual(response.status_code, 404)
+        e.allow_route_upload = True
+        e.save()
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-1/route-upload")
+        self.assertEqual(response.status_code, 200)
+
+    def test_future_event_pages_loads(self):
+        Event.objects.create(
+            name="Kiila Cup 2",
+            slug="kiila-cup-2",
+            club=self.club,
+            start_date=arrow.now().shift(hours=1).datetime,
+            end_date=arrow.now().shift(hours=2).datetime,
+            open_registration=True,
+            allow_route_upload=True,
+        )
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-2")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-2/export")
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-2/registration")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-2/route-upload")
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_event_pages_loads(self):
+        Event.objects.create(
+            name="Kiila Cup 3",
+            slug="kiila-cup-3",
+            club=self.club,
+            start_date=arrow.now().shift(hours=-2).datetime,
+            end_date=arrow.now().shift(hours=-1).datetime,
+            open_registration=True,
+            allow_route_upload=True,
+        )
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-3")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-3/export")
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-3/registration")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Sorry, Registration is closed")
+
+        response = self.client.get(f"{self.live_server_url}/kiila-cup-3/route-upload")
+        self.assertEqual(response.status_code, 200)
+
+    # TODO: Test private events visibility
