79c5f83b374de2d28d3d0a936df0f295e1dd9203
Raphael Stefanini <raphael@rphlo.com>
2022-05-25 20:34:07 +0300

    Fix gps seuranta import

--- routechoices/core/bg_tasks.py
+++ routechoices/core/bg_tasks.py
@@ -304,8 +304,8 @@ def import_single_event_from_gps_seuranta(event_id):
         slug=event_id,
         defaults={
             "name": event_data["RACENAME"],
-            "start_date": event_start_date,
-            "end_date": event_end_date,
+            "start_date": arrow.get(event_start_date).datetime,
+            "end_date": arrow.get(event_end_date).datetime,
         },
     )
     if not created:
