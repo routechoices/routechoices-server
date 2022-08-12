7a13bd37408a569a6dfd0c1d18b9e6263b2ba364
Raphael Stefanini <raphael@rphlo.com>
2022-08-12 07:18:52 +0300

    Faster tiles

--- routechoices/core/models.py
+++ routechoices/core/models.py
@@ -592,9 +592,9 @@ class Map(models.Model):
         )
         coeffs = cv2.getPerspectiveTransform(p1, p2)
         orig = self.data
-        img = Image.open(BytesIO(orig)).convert("RGBA")
-        img_alpha = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGRA)
-
+        img_nparr = np.fromstring(orig, np.uint8)
+        img = cv2.imdecode(img_nparr, cv2.IMREAD_COLOR)
+        img_alpha = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2BGRA)
         tile_img = cv2.warpPerspective(
             img_alpha,
             coeffs,
@@ -609,7 +609,7 @@ class Map(models.Model):
         elif img_mime == "image/jpeg":
             extra_args = [int(cv2.IMWRITE_JPEG_QUALITY), 95]
         if img_mime == "image/avif":
-            color_coverted = cv2.cvtColor(tile_img, cv2.COLOR_RGBA2BGRA)
+            color_coverted = cv2.cvtColor(tile_img, cv2.COLOR_BGRA2RGBA)
             pil_image = Image.fromarray(color_coverted)
             buffer = BytesIO()
             pil_image.save(buffer, "AVIF", quality=80)
