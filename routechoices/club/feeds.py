0f4b808cf3b5a77dbdf5a50f0a5490725bb3c7c6
Raphael Stefanini <raphael@rphlo.com>
2022-08-25 11:28:36 +0300

    Update RSS feeds to not display future events

--- routechoices/club/feeds.py
+++ routechoices/club/feeds.py
@@ -1,4 +1,5 @@
 from django.contrib.syndication.views import Feed
+from django.utils.timezone import now
 
 from routechoices.core.models import PRIVACY_PUBLIC, Club, Event
 
@@ -8,25 +9,29 @@ class ClubLiveEventsFeed(Feed):
         return Club.objects.get(slug__iexact=request.club_slug)
 
     def title(self, obj):
-        return f"Live GPS Events by {obj.name}"
+        return f"GPS Tracking Events by {obj.name}"
 
     def description(self, obj):
-        return f"Events by {obj.name}"
+        return f"Watch live or later the GPS Tracking Events by {obj.name}"
 
     def link(self, obj):
         return f"{obj.nice_url}"
 
     def items(self, obj):
-        return Event.objects.select_related("club").filter(
-            privacy=PRIVACY_PUBLIC,
-            club=obj,
-        )[:25]
+        return (
+            Event.objects.select_related("club")
+            .filter(
+                privacy=PRIVACY_PUBLIC,
+                club=obj,
+            )
+            .filter(start_date__lte=now())
+        )
 
     def item_title(self, item):
         return item.name
 
     def item_description(self, item):
-        return f"Live GPS Tracking of {item.name} by {item.club}"
+        return f"GPS Tracking of {item.name} by {item.club}"
 
     def item_pubdate(self, item):
         return item.start_date
--- routechoices/site/feeds.py
+++ routechoices/site/feeds.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.contrib.syndication.views import Feed
+from django.utils.timezone import now
 from django_hosts.resolvers import reverse
 
 from routechoices.core.models import PRIVACY_PUBLIC, Event
@@ -9,25 +10,29 @@ from routechoices.core.models import PRIVACY_PUBLIC, Event
 class LiveEventsFeed(Feed):
     def title(self):
         site = Site.objects.get(id=settings.SITE_ID)
-        return f"Live GPS Events on {site.name}"
+        return f"GPS Tracking Events on {site.name}"
 
     def description(self):
         site = Site.objects.get(id=settings.SITE_ID)
-        return f"Events on {site.name}"
+        return f"Watch live or later the GPS Tracking Events on {site.name}"
 
     def link(self):
         return reverse("site:events_view")
 
     def items(self):
-        return Event.objects.select_related("club").filter(
-            privacy=PRIVACY_PUBLIC,
-        )[:25]
+        return (
+            Event.objects.select_related("club")
+            .filter(
+                privacy=PRIVACY_PUBLIC,
+            )
+            .filter(start_date__lte=now())
+        )
 
     def item_title(self, item):
         return item.name
 
     def item_description(self, item):
-        return f"Live GPS Tracking of {item.name} by {item.club}"
+        return f"GPS Tracking of {item.name} by {item.club}"
 
     def item_pubdate(self, item):
         return item.start_date
