4ed3910534a300596dbebaab1553a4eb015f1c59
Raphaël Stefanini <rphlo@users.noreply.github.com>
2022-09-01 15:38:40 +0300

    Add timeout to post request that transfer data to live push server

--- routechoices/core/models.py
+++ routechoices/core/models.py
@@ -1237,6 +1237,7 @@ class Device(models.Model):
                             headers={
                                 "Authorization": f"Bearer {settings.LIVESTREAM_INTERNAL_SECRET}"
                             },
+                            timeout=1
                         )
             except Exception:
                 pass
