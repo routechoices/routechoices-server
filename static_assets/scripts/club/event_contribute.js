3495531c4f5b5def5669ebd589e4a6b7877766d7
Raphael Stefanini <raphael@rphlo.com>
2022-10-14 10:17:33 +0300

    Uniformize UI

--- static_assets/scripts/club/event_contribute.js
+++ static_assets/scripts/club/event_contribute.js
@@ -276,5 +276,12 @@ function selectizeDeviceInput() {
         },
       });
     });
+    if (
+      u("#upload-form").find("#id_competitor_aid").nodes[0].options.length === 0
+    ) {
+      u("#upload-form").html(
+        "<h3 style='color:rgb(0,0,0,0.6)'>No competitors to upload data to.</h3>"
+      );
+    }
   }
 })();
