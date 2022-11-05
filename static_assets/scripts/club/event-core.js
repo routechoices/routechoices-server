38d27300aa7a33a06ff034edd34c9aaa4c330a8f
Raphaël Stefanini <raphael@rphlo.com>
2022-11-05 07:31:50 +0000

    lint

--- static_assets/scripts/club/event-core.js
+++ static_assets/scripts/club/event-core.js
@@ -2320,13 +2320,13 @@ L.Control.EventState = L.Control.extend({
   },
   hide() {
     if (!this._div) {
-      return
+      return;
     }
     this._div.style.display = "none";
   },
   setLive() {
     if (!this._div) {
-      return
+      return;
     }
     this._div.innerHTML =
       '<svg style="color: #fff;margin-top: -5px;margin-left: -10px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" preserveAspectRatio="xMidYMid meet" x="955"  stroke="#fff" width="20"><g fill="none" fill-rule="evenodd" stroke-width="2"><circle cx="22" cy="22" r="1"><animate attributeName="r" begin="0s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/><animate attributeName="stroke-opacity" begin="0s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/></circle><circle cx="22" cy="22" r="1"><animate attributeName="r" begin="-0.9s" dur="1.8s" values="1; 20" calcMode="spline" keyTimes="0; 1" keySplines="0.165, 0.84, 0.44, 1" repeatCount="indefinite"/><animate attributeName="stroke-opacity" begin="-0.9s" dur="1.8s" values="1; 0" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1" repeatCount="indefinite"/></circle></g></svg> ' +
