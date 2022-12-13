3b0a48825029b9a85d7c247159ed664ecfdea3a7
Raphael Stefanini <raphael@rphlo.com>
2022-12-13 15:30:51 +0200

    Swedish translation of replay

--- static_assets/scripts/club/event-core.js
+++ static_assets/scripts/club/event-core.js
@@ -2350,7 +2350,7 @@ function shareUrl(e) {
 function updateText() {
   banana.setLocale(locale);
   var langFile = `${window.local.staticRoot}i18n/club/event/${locale}.json`;
-  return fetch(`${langFile}?v=2022120300`)
+  return fetch(`${langFile}?v=2022121300`)
     .then((response) => response.json())
     .then((messages) => {
       banana.load(messages, banana.locale);
