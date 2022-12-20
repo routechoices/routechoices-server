4885f8cbf9efd01a703a25441ce15c4d3030778e
Raphael Stefanini <raphael@rphlo.com>
2022-12-20 13:43:28 +0200

    Remove old data

--- routechoices/core/models.py
+++ routechoices/core/models.py
@@ -1105,7 +1105,6 @@ class Device(models.Model):
         related_name="devices",
         through_fields=("device", "club"),
     )
-    locations_encoded = models.TextField(blank=True, default="")
     locations_encoded_compressed = models.BinaryField(blank=True, default=b"")
     battery_level = models.PositiveIntegerField(
         null=True, default=None, validators=[MaxValueValidator(100)], blank=True
@@ -1171,32 +1170,32 @@ class Device(models.Model):
         return min(4, round((self.battery_level - 5) / 20))
 
     @property
-    def locations_encoded_(self):
+    def locations_encoded(self):
         if not self.locations_encoded_compressed:
             return ""
         return zstd.decompress(bytes(self.locations_encoded_compressed)).decode()
 
-    @locations_encoded_.setter
-    def locations_encoded_(self, data):
+    @locations_encoded.setter
+    def locations_encoded(self, data):
         self.locations_encoded_compressed = zstd.compress(data.encode())
 
     @property
     def locations_series(self):
-        if not self.locations_encoded_:
+        if not self.locations_encoded:
             return []
-        return gps_encoding.decode_data(self.locations_encoded_)
+        return gps_encoding.decode_data(self.locations_encoded)
 
     @locations_series.setter
     def locations_series(self, locations_list):
         sorted_locations = list(
             sorted(locations_list, key=itemgetter(LOCATION_TIMESTAMP_INDEX))
         )
-        self.locations_encoded_ = gps_encoding.encode_data(sorted_locations)
+        self.locations_encoded = gps_encoding.encode_data(sorted_locations)
         self.update_cached_data()
 
     @property
     def locations(self):
-        if not self.locations_encoded_:
+        if not self.locations_encoded:
             return {"timestamps": [], "latitudes": [], "longitudes": []}
         locs = self.locations_series
         data = list(zip(*locs))
@@ -1312,7 +1311,7 @@ class Device(models.Model):
     def location_count(self):
         # This use a property of the GPS encoding format
         n = 0
-        for x in self.locations_encoded_:
+        for x in self.locations_encoded:
             if ord(x) - 63 < 0x20:
                 n += 1
         return n // 3
