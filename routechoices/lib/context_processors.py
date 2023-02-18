58e36a9ae7c7c454f6c3cd404acbef217499c3b3
Raphael Stefanini <raphael@rphlo.com>
2023-02-18 12:27:19 +0200

    Rid of site version

--- routechoices/lib/context_processors.py
+++ routechoices/lib/context_processors.py
@@ -1,31 +1,9 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
 
-try:
-    from git import Repo
-except ImportError:
-
-    class Repo:
-        def __init__(self, *args, **kwargs):
-            raise NotImplementedError()
-
-
-def get_git_revision_hash():
-    try:
-        repo = Repo(settings.BASE_DIR)
-    except NotImplementedError:
-        return "dev"
-    hc = repo.head.commit
-    return hc.hexsha
-
-
-def get_git_revision_short_hash():
-    return get_git_revision_hash()[:7]
-
 
 def site(request):
     return {
         "site": Site.objects.get_current(),
         "enable_analytics": getattr(settings, "ENABLE_ANALYTICS", None),
-        "version": get_git_revision_hash(),
     }
