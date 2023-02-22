94ac0230ce5bbcfc7f9c0cc6a60f3b02f5da96a8
Raphael Stefanini <raphael@rphlo.com>
2023-02-22 15:07:40 +0200

    Link to quick event page

---
+++ static_assets/scripts/dashboard/tooltips.js
@@ -0,0 +1,8 @@
+(function () {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})();
