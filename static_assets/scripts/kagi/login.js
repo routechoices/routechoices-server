989e8bed1054e5497429c4d7d9605ca6cb296e80
Raphaël Stefanini <raphael@rphlo.com>
2023-03-22 12:35:59 +0000

    precommit

--- static_assets/scripts/kagi/login.js
+++ static_assets/scripts/kagi/login.js
@@ -1,12 +1,12 @@
 (function () {
-  u("input[type='password']")
-    .wrap('<div class="input-group mb-3"></div>')
+  u("input[type='password']").wrap('<div class="input-group mb-3"></div>');
   u("input[type='password']").after(
-      '<span class="input-group-text togglePassword" style="cursor: pointer"><i class="fa-regular fa-fw fa-eye"></i></span>'
-    )
+    '<span class="input-group-text togglePassword" style="cursor: pointer"><i class="fa-regular fa-fw fa-eye"></i></span>'
+  );
   u(".togglePassword").on("click", function (e) {
-    u(e.target).parent().find('i').toggleClass("fa-eye-slash, fa-eye");
-    u(e.target).parent()
+    u(e.target).parent().find("i").toggleClass("fa-eye-slash, fa-eye");
+    u(e.target)
+      .parent()
       .parent()
       .find("input")
       .each(function (el) {
