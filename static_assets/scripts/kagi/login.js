fd24bf2a892a6ffe58ef7590086a6bf086e1ddc3
Raphaël Stefanini <raphael@rphlo.com>
2023-03-22 11:40:52 +0000

    test

---
+++ static_assets/scripts/kagi/login.js
@@ -0,0 +1,14 @@
+(function () {
+  u("input[type='password']").after(
+    '<i class="fa-regulat fa-eye-slash togglePassword"></i>'
+  );
+  u(".togglePassword").on("click", function (e) {
+    u(e.target).toggleClass("fa-eye", "fa-eye-slash");
+    u(e.target)
+      .parent()
+      .find("input")
+      .each(function (el) {
+        el.type = el.type == "password" ? "text" : "password";
+      });
+  });
+})();
