b416c32931fe319a52baff526de88c109fe0811c
Raphaël Stefanini <rphlo@users.noreply.github.com>
2023-03-22 14:13:56 +0200

    Update login.js

--- static_assets/scripts/kagi/login.js
+++ static_assets/scripts/kagi/login.js
@@ -1,7 +1,9 @@
 (function () {
-  u("input[type='password']").after(
-    '<span class="input-group-text togglePassword"><i class="fa-regular fa-eye-slash"></i></span>'
-  ).wrap('<div class="input-group mb-3"></div>');
+  u("input[type='password']")
+    .wrap('<div class="input-group mb-3"></div>')
+    .after(
+      '<span class="input-group-text togglePassword"><i class="fa-regular fa-eye-slash"></i></span>'
+    )
   u(".togglePassword").on("click", function (e) {
     u(e.target).find('i').toggleClass("fa-eye", "fa-eye-slash");
     u(e.target)
