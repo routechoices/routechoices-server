69a607060c642341b158803ccbca6a8a627ad14a
Raphaël Stefanini <rphlo@users.noreply.github.com>
2023-03-22 14:04:06 +0200

    Update login.js

--- static_assets/scripts/kagi/login.js
+++ static_assets/scripts/kagi/login.js
@@ -1,6 +1,6 @@
 (function () {
-  /*  u("input[type='password']").after(
-    '<i class="fa-regulat fa-eye-slash togglePassword"></i>'
+  u("input[type='password']").after(
+    '<span class="input-group-text"><i class="fa-regular fa-eye-slash togglePassword"></i></span>'
   );
   u(".togglePassword").on("click", function (e) {
     u(e.target).toggleClass("fa-eye", "fa-eye-slash");
@@ -11,5 +11,4 @@
         el.type = el.type == "password" ? "text" : "password";
       });
   });
-*/
 })();
