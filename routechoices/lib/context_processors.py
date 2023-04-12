8a8ee696685ec073b32a4362fda0172c06aabe08
Raphael Stefanini <raphael@rphlo.com>
2023-04-12 13:06:12 +0300

    Avoid query in context processor

--- routechoices/lib/context_processors.py
+++ routechoices/lib/context_processors.py
@@ -1,9 +1,15 @@
+from functools import cache
+
 from django.conf import settings
 from django.contrib.sites.models import Site
 
 
 def site(request):
+    @cache
+    def current_site():
+        return Site.objects.get_current()
+
     return {
-        "site": Site.objects.get_current(),
+        "site": current_site,
         "enable_analytics": getattr(settings, "ENABLE_ANALYTICS", None),
     }
