7bae542d44c9ac96f1558d05d9f7649860cf2568
Raphael Stefanini <raphael@rphlo.com>
2023-05-03 18:11:54 +0300

    Bump django version

--- generate-fontawesome-subset.js
+++ generate-fontawesome-subset.js
@@ -1,7 +1,9 @@
 // npm install --save-dev fontawesome-subset
 // npm install --save-dev @fortawesome/fontawesome-free
-
+// npm install --save-dev sass
+const fs = require("fs");
 const { fontawesomeSubset } = require("fontawesome-subset");
+const sass = require("sass");
 
 fontawesomeSubset(
   {
@@ -75,3 +77,17 @@ fontawesomeSubset(
   },
   "sass/webfonts"
 );
+
+var a = sass.compile(
+  "static_assets/vendor/fontawesome-free-6.4.0-web/scss/fontawesome.scss"
+).css;
+var b = sass.compile(
+  "static_assets/vendor/fontawesome-free-6.4.0-web/scss/solid.scss"
+).css;
+var c = sass.compile(
+  "static_assets/vendor/fontawesome-free-6.4.0-web/scss/regular.scss"
+).css;
+var d = sass.compile(
+  "static_assets/vendor/fontawesome-free-6.4.0-web/scss/brands.scss"
+).css;
+fs.writeFileSync("all.css", [a, b, c, d].join("\n"));
