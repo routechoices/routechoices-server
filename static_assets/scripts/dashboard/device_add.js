cec8aedf37e47b5423c6561637ca06a92fca4551
Raphael Stefanini <raphael@rphlo.com>
2023-06-13 10:27:23 +0300

    Allow batch import of devices by device id

--- static_assets/scripts/dashboard/device_add.js
+++ static_assets/scripts/dashboard/device_add.js
@@ -25,62 +25,101 @@ function onCsvParsed(result) {
       if (!empty && l.length != 2) {
         errors += "All rows should have 2 columns\n";
       } else {
-        var imei = l[0];
+        var imeiOrDeviceId = l[0];
         var nickname = l[1];
         if (nickname.length > 12) {
           errors +=
-            "IMEI " +
-            imei +
+            "IMEI or DeviceID " +
+            imeiOrDeviceId +
             ', nickname "' +
             nickname +
             '": nickname too long (Max 12 characters)\n';
           return;
         }
-        reqwest({
-          url: window.local.apiBaseUrl + "device/",
-          method: "post",
-          data: {
-            imei: imei,
-            csrfmiddlewaretoken: window.local.csrfToken,
-          },
-          type: "json",
-          withCredentials: true,
-          crossOrigin: true,
-        })
-          .then(function (resp) {
-            var deviceId = resp.device_id;
-            reqwest({
-              url:
-                window.local.apiBaseUrl +
-                "clubs/" +
-                window.local.clubId +
-                "/devices/" +
-                deviceId +
-                "/",
-              method: "patch",
-              data: {
-                nickname: nickname,
-              },
-              headers: { "X-CSRFToken": window.local.csrfToken },
-              type: "json",
-              withCredentials: true,
-              crossOrigin: true,
-            })
-              .then(function () {
-                nImported++;
-                displaySuccess(nImported + " devices imported");
-              })
-              .fail(function () {
-                errors +=
-                  "IMEI " + imei + ', nickname "' + nickname + '": Failure\n';
-                displayErrors(errors);
-              });
+        if (imeiOrDeviceId.length === 8) {
+          var deviceId = imeiOrDeviceId;
+          reqwest({
+            url:
+              window.local.apiBaseUrl +
+              "clubs/" +
+              window.local.clubId +
+              "/devices/" +
+              deviceId +
+              "/",
+            method: "patch",
+            data: {
+              nickname: nickname,
+            },
+            headers: { "X-CSRFToken": window.local.csrfToken },
+            type: "json",
+            withCredentials: true,
+            crossOrigin: true,
           })
-          .fail(function () {
-            errors +=
-              "IMEI " + imei + ', nickname "' + nickname + '": Invalid IMEI\n';
-            displayErrors(errors);
-          });
+            .then(function () {
+              nImported++;
+              displaySuccess(nImported + " devices imported");
+            })
+            .fail(function () {
+              errors +=
+                "Device ID " +
+                deviceId +
+                ', nickname "' +
+                nickname +
+                '": Failure\n';
+              displayErrors(errors);
+            });
+        } else {
+          var imei = imeiOrDeviceId;
+          reqwest({
+            url: window.local.apiBaseUrl + "device/",
+            method: "post",
+            data: {
+              imei: imei,
+              csrfmiddlewaretoken: window.local.csrfToken,
+            },
+            type: "json",
+            withCredentials: true,
+            crossOrigin: true,
+          })
+            .then(function (resp) {
+              var deviceId = resp.device_id;
+              reqwest({
+                url:
+                  window.local.apiBaseUrl +
+                  "clubs/" +
+                  window.local.clubId +
+                  "/devices/" +
+                  deviceId +
+                  "/",
+                method: "patch",
+                data: {
+                  nickname: nickname,
+                },
+                headers: { "X-CSRFToken": window.local.csrfToken },
+                type: "json",
+                withCredentials: true,
+                crossOrigin: true,
+              })
+                .then(function () {
+                  nImported++;
+                  displaySuccess(nImported + " devices imported");
+                })
+                .fail(function () {
+                  errors +=
+                    "IMEI " + imei + ', nickname "' + nickname + '": Failure\n';
+                  displayErrors(errors);
+                });
+            })
+            .fail(function () {
+              errors +=
+                "IMEI " +
+                imei +
+                ', nickname "' +
+                nickname +
+                '": Invalid IMEI\n';
+              displayErrors(errors);
+            });
+        }
       }
     });
   }
