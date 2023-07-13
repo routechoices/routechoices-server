3edb139399c2602885736d28cbec51dbd8eddd41
Raphael Stefanini <raphael@rphlo.com>
2023-07-13 10:22:27 +0300

    Add info about number of maps in KMZ before upload

--- static_assets/scripts/dashboard/map_kmz_upload.js
+++ static_assets/scripts/dashboard/map_kmz_upload.js
@@ -1,7 +1,7 @@
 (function () {
   var $field = u("#id_file");
   $field.attr("accept", ".kml, .kmz");
-  $field.on("change", function () {
+  $field.on("change", async function () {
     if (this.files.length > 0 && this.files[0].size > 2 * 1e7) {
       swal({
         title: "Error!",
@@ -10,6 +10,37 @@
         confirmButtonText: "OK",
       });
       this.value = "";
+    }
+    if (this.files.length > 0) {
+      try {
+        var zip = await JSZip.loadAsync(this.files[0]);
+        if (zip.files && zip.files["doc.kml"]) {
+          const kml = await zip.file("doc.kml").async("string");
+          const parser = new DOMParser();
+          const parsedText = parser.parseFromString(kml, "text/xml");
+          const nLayers =
+            parsedText.getElementsByTagName("GroundOverlay").length;
+          swal({
+            title: "Info",
+            text:
+              "File contains " +
+              nLayers +
+              " map" +
+              (nLayers != 1 ? "s" : "") +
+              "!",
+            type: "info",
+            confirmButtonText: "OK",
+          });
+        }
+      } catch {
+        swal({
+          title: "Error!",
+          text: "Invalid KMZ!",
+          type: "error",
+          confirmButtonText: "OK",
+        });
+        this.value = "";
+      }
     }
   });
 })();
