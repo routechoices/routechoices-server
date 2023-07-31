0d2fccf6bc6803b7d97b9d1820b2047317584ca1
Raphael Stefanini <raphael@rphlo.com>
2023-07-31 14:24:59 +0300

    Add html minification

--- routechoices/settings.py
+++ routechoices/settings.py
@@ -86,6 +86,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_minify_html.middleware.MinifyHtmlMiddleware",
 ]
 
 
