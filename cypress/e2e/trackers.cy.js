5ba03f529cb737d6290dd71deff0e92e254087ee
Raphael Stefanini <raphael@rphlo.com>
2023-08-09 17:54:31 +0300

    Tweak trackers page

--- cypress/e2e/trackers.cy.js
+++ cypress/e2e/trackers.cy.js
@@ -1,20 +1,32 @@
 context("IMEI device id generation", () => {
   it("Create an Device Id", function () {
     cy.visit("/trackers");
+    cy.get("#hardware-tab-btn").click();
+
+    // Invalid too short IMEI
     cy.get("#IMEI").clear().type("0123456789");
     cy.get("#submit-btn").click();
     cy.contains("Invalid IMEI (must be 15 digits)");
     cy.get("#copyDevIdBtn").should("not.be.visible");
 
+    // Invalid too long IMEI
+    cy.get("#IMEI").clear().type("0123456789012345");
+    cy.get("#submit-btn").click();
+    cy.contains("Invalid IMEI (must be 15 digits)");
+    cy.get("#copyDevIdBtn").should("not.be.visible");
+
+    // Invalid: Luhn check
     cy.get("#IMEI").clear().type("012345678901234");
     cy.get("#submit-btn").click();
     cy.contains("Invalid IMEI (check digit does not match)");
     cy.get("#copyDevIdBtn").should("not.be.visible");
 
+    // Valid
     cy.get("#IMEI").clear().type("012345678901237");
     cy.get("#submit-btn").click();
     cy.get("#copyDevIdBtn").should("be.visible");
 
+    // Invalid: Contains a letter           v this is letter "O"
     cy.get("#IMEI").clear().type("0123456789O1234");
     cy.get("#submit-btn").click();
     cy.contains("Invalid IMEI (must be 15 digits)");
