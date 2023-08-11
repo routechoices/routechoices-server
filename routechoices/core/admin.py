1bed038cb30d65fd76c26cd8df7f3657ec00c35e
Raphaël Stefanini <rphlo@users.noreply.github.com>
2023-08-11 16:51:26 +0300

    Update admin.py

--- routechoices/core/admin.py
+++ routechoices/core/admin.py
@@ -428,7 +428,7 @@ class MapAdmin(admin.ModelAdmin):
         "creation_date",
         "resolution",
         "max_zoom",
-        "rotation",
+        "north_rotation",
     )
     list_filter = ("club",)
     list_select_related = ("club",)
