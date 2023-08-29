3c7f0dd7c91dde9565fadccc1be6be6a82914a65
Raphael Stefanini <raphael@rphlo.com>
2023-08-29 12:28:05 +0300

    Improve image cleanup command

--- routechoices/core/management/commands/remove_unused_images.py
+++ routechoices/core/management/commands/remove_unused_images.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from routechoices.core.models import Map
+from routechoices.core.models import Club, Map
 from routechoices.lib.s3 import get_s3_client, s3_delete_key
 
 
@@ -11,13 +11,13 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--force", action="store_true", default=False)
 
-    def scan_map_directory(self):
+    def scan_directory(self, directory):
         # Should use v2 but wasabi fails to list all files with it
         # paginator = s3.get_paginator('list_objects_v2')
         paginator = self.s3.get_paginator("list_objects")
         kwargs = {
             "Bucket": settings.AWS_S3_BUCKET,
-            "Prefix": "maps",
+            "Prefix": directory,
         }
         for page in paginator.paginate(**kwargs):
             try:
@@ -29,24 +29,43 @@ class Command(BaseCommand):
                 yield key
 
     def process_image_file(self, image_name, force):
-        if image_name not in self.maps_image_paths:
+        if image_name not in self.image_paths:
             self.n_image_removed += 1
             self.stdout.write(f"File {image_name} is unused")
             if force:
                 s3_delete_key(image_name, settings.AWS_S3_BUCKET)
         else:
+            self.stdout.write(f"File {image_name} is used")
             self.n_image_keeped += 1
             self.keeped.add(image_name)
 
     def handle(self, *args, **options):
         force = options["force"]
-        self.maps_image_paths = set(Map.objects.all().values_list("image", flat=True))
+        self.image_paths = set(Map.objects.all().values_list("image", flat=True))
+        self.image_paths.update(
+            set(
+                Club.objects.all()
+                .exclude(logo__isnull=True)
+                .exclude(logo="")
+                .values_list("logo", flat=True)
+            )
+        )
+        self.image_paths.update(
+            set(
+                Club.objects.all()
+                .exclude(banner__isnull=True)
+                .exclude(banner="")
+                .values_list("banner", flat=True)
+            )
+        )
+
         self.n_image_removed = 0
         self.keeped = set()
         self.n_image_keeped = 0
         self.s3 = get_s3_client()
-        for filename in self.scan_map_directory():
-            self.process_image_file(filename, force)
+        for directory in ("maps", "logos", "banners"):
+            for filename in self.scan_directory(directory):
+                self.process_image_file(filename, force)
 
         if force:
             self.stdout.write(
