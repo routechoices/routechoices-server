d41f63f32e602481ee4211ccd1c3f13e6c7bddd8
Raphael Stefanini <raphael@rphlo.com>
2023-10-20 15:29:38 +0300

    Fix

--- routechoices/settings.py
+++ routechoices/settings.py
@@ -19,9 +19,7 @@ from routechoices.slug_blacklist import SLUG_BLACKLIST
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
