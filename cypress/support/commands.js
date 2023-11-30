7cec0c017e9d521f539c1fb5583db69c2229bdd2
Raphael Stefanini <raphael@rphlo.com>
2023-11-30 16:08:02 +0200

    Dont use id for submit btn in e2e tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -63,7 +63,7 @@ Cypress.Commands.add("createMap", () => {
   const mapFileName =
     "Jukola_1st_leg_blank_61.45075_24.18994_61.44656_24.24721_61.42094_24.23851_61.42533_24.18156_.jpg";
   cy.get("#id_image").selectFile("cypress/fixtures/" + mapFileName);
-  cy.get("form button:not([type]),form button[type=submit]").click();
+  cy.get("#submit-btn").click();
   cy.url().should("match", /\/dashboard\/maps$/);
 });
 
