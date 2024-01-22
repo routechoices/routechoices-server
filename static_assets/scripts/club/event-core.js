954439a960188f18ea3081cf508f8675b6409706
Raphael Stefanini <raphael@rphlo.com>
2024-01-22 13:57:17 +0200

    Correct translation

--- static_assets/scripts/club/event-core.js
+++ static_assets/scripts/club/event-core.js
@@ -394,7 +394,7 @@ function sortingFunction(a, b) {
 function updateText() {
   banana.setLocale(locale);
   var langFile = `${window.local.staticRoot}i18n/club/event/${locale}.json`;
-  return fetch(`${langFile}?v=2024012100`)
+  return fetch(`${langFile}?v=2024012200`)
     .then((response) => response.json())
     .then((messages) => {
       banana.load(messages, banana.locale);
