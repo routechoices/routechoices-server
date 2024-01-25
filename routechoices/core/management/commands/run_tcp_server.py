24d775fa4cb72924914951904087be5ee3f0b40e
Raphael Stefanini <raphael@rphlo.com>
2024-01-25 14:59:07 +0200

    TK201 support

--- routechoices/core/management/commands/run_tcp_server.py
+++ routechoices/core/management/commands/run_tcp_server.py
@@ -525,6 +525,106 @@ class TrackTapeConnection:
         print("client quit", flush=True)
 
 
+class TK201Connection:
+    def __init__(self, stream, address):
+        print(f"received a new connection from {address} on port 2004")
+        self.aid = random_key()
+        self.imei = None
+        self.address = address
+        self.stream = stream
+        self.stream.set_close_callback(self._on_close)
+        self.db_device = None
+
+    async def start_listening(self):
+        print(f"Start listening from {self.address}")
+        imei = None
+        try:
+            data_raw = ""
+            while not data_raw:
+                data_bin = await self.stream.read_until(b"\n")
+                data_raw = data_bin.decode("ascii").strip()
+            print(f"Received data ({data_raw})", flush=True)
+            data = data_raw.split(",")
+            imei = data[17].split(":")[1]
+        except Exception as e:
+            print(e, flush=True)
+            self.stream.close()
+            return
+        if not imei:
+            print("No imei", flush=True)
+            self.stream.close()
+            return
+        is_valid_imei = True
+        try:
+            validate_imei(imei)
+        except ValidationError:
+            is_valid_imei = False
+        if not is_valid_imei:
+            print("Invalid imei", flush=True)
+            self.stream.close()
+            return
+        self.db_device = await sync_to_async(_get_device, thread_sensitive=True)(imei)
+        if not self.db_device:
+            print(f"Imei not registered {self.address}, {imei}", flush=True)
+            self.stream.close()
+            return
+        self.imei = imei
+        print(f"{self.imei} is connected")
+
+        await self._process_data(data)
+
+        while await self._read_line():
+            pass
+
+    async def _process_data(self, data):
+        if not self.db_device.user_agent:
+            self.db_device.user_agent = "TrackTape"
+        imei = data[17].split(":")[1]
+        if imei != self.imei:
+            return False
+        logger.info(
+            f"{arrow.now().datetime}, TK201 DATA, "
+            f"{self.aid}, {self.address}: {','.join(data)}"
+        )
+        try:
+            tim = arrow.get(f"{data[11]} {data[3][:6]}", "DDMMYY HHmmss").int_timestamp
+            lat_minute = float(data[5])
+            lat = lat_minute // 100 + (lat_minute % 100) / 60
+            if data[6] == "S":
+                lat *= -1
+            lon_minute = float(data[7])
+            lon = lon_minute // 100 + (lon_minute % 100) / 60
+            if data[8] == "W":
+                lon *= -1
+        except Exception:
+            print("Could not parse GPS data", flush=True)
+        else:
+            loc_array = [(tim, lat, lon)]
+            if data[4] == "A":
+                await sync_to_async(
+                    self.db_device.add_locations, thread_sensitive=True
+                )(loc_array)
+                print(f"{len(loc_array)} locations wrote to DB", flush=True)
+
+    async def _read_line(self):
+        try:
+            data_raw = ""
+            while not data_raw:
+                data_bin = await self.stream.read_until(b"\n")
+                data_raw = data_bin.decode("ascii").strip()
+            print(f"Received data ({data_raw})")
+            data = data_raw.split(",")
+            await self._process_data(data)
+        except Exception as e:
+            print(f"Error parsing data: {str(e)}")
+            self.stream.close()
+            return False
+        return True
+
+    def _on_close(self):
+        print("client quit", flush=True)
+
+
 class MicTrackConnection:
     def __init__(self, stream, address):
         print(f"Received a new connection from {address} on port 2001")
@@ -801,6 +901,10 @@ class TrackTapeServer(GenericTCPServer):
     connection_class = TrackTapeConnection
 
 
+class TK201Server(GenericTCPServer):
+    connection_class = TK201Connection
+
+
 class Command(BaseCommand):
     help = "Run a TCP server for GPS trackers."
 
@@ -817,6 +921,9 @@ class Command(BaseCommand):
         parser.add_argument(
             "--tracktape-port", nargs="?", type=int, help="Tracktape Handler Port"
         )
+        parser.add_argument(
+            "--tk201-port", nargs="?", type=int, help="TK201 Handler Port"
+        )
 
     def handle(self, *args, **options):
         signal.signal(signal.SIGTERM, sigterm_handler)
@@ -832,6 +939,9 @@ class Command(BaseCommand):
         if options.get("tracktape_port"):
             tracktape_server = TrackTapeServer()
             tracktape_server.listen(options["tracktape_port"])
+        if options.get("tk201_port"):
+            tk201_server = TK201Server()
+            tk201_server.listen(options["tk201_port"])
         try:
             print("Start listening TCP data...", flush=True)
             logger.info(f"{arrow.now().datetime}, UP")
@@ -845,6 +955,8 @@ class Command(BaseCommand):
                 mictrack_server.stop()
             if options.get("tracktape_port"):
                 tracktape_server.stop()
+            if options.get("tk201_port"):
+                tk201_server.stop()
             IOLoop.current().stop()
         finally:
             print("Stopped listening TCP data...", flush=True)
