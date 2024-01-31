b36325e42680f6105c4afd39c72cf1305590182b
Raphael Stefanini <raphael@rphlo.com>
2024-01-31 11:01:43 +0200

    Improve luhn checker

--- routechoices/lib/luhn.py
+++ routechoices/lib/luhn.py
@@ -5,7 +5,7 @@ def checksum(string):
     """
     digits = list(map(int, string))
     odd_sum = sum(digits[-1::-2])
-    even_sum = sum([sum(divmod(2 * d, 10)) for d in digits[-2::-2]])
+    even_sum = sum(sum(divmod(2 * d, 10)) for d in digits[-2::-2])
     return (odd_sum + even_sum) % 10
 
 
