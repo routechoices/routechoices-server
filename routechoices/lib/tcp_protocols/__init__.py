684fd72c598fb5bd9444fe90de35440322754c5c
Raphael Stefanini <raphael@rphlo.com>
2024-01-31 18:27:30 +0200

    Refactor TCP protocols

---
+++ routechoices/lib/tcp_protocols/__init__.py
@@ -0,0 +1,13 @@
+from routechoices.lib.tcp_protocols.mictrack import MicTrackConnection
+from routechoices.lib.tcp_protocols.queclink import QueclinkConnection
+from routechoices.lib.tcp_protocols.tk201 import TK201Connection
+from routechoices.lib.tcp_protocols.tmt250 import TMT250Connection
+from routechoices.lib.tcp_protocols.tracktape import TrackTapeConnection
+
+__all__ = (
+    MicTrackConnection,
+    QueclinkConnection,
+    TK201Connection,
+    TMT250Connection,
+    TrackTapeConnection,
+)
