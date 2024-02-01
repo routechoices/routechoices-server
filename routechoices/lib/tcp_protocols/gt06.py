0f927dfd7f2c48e5779a36d058c56738cb8b0c48
Raphael Stefanini <raphael@rphlo.com>
2024-02-01 07:30:44 +0200

    Improve gt06 protocol to include heartbeat and alarm

--- routechoices/lib/tcp_protocols/gt06.py
+++ routechoices/lib/tcp_protocols/gt06.py
@@ -70,6 +70,7 @@ class GT06Connection:
         )
         if not self.db_device.user_agent:
             self.db_device.user_agent = "Gt06"
+
         date_bin = data_bin[4:10]
         lat_bin = data_bin[11:15]
         lon_bin = data_bin[15:19]
@@ -88,6 +89,9 @@ class GT06Connection:
             lat *= -1
         if west:
             lon *= -1
+
+        is_alarm = data_bin[3] == 0x26
+
         if flags & 0x16:
             loc_array = [(arrow.get(date_str).timestamp(), lat, lon)]
             await sync_to_async(self.db_device.add_locations, thread_sensitive=True)(
@@ -95,13 +99,31 @@ class GT06Connection:
             )
             print("1 locations wrote to DB", flush=True)
 
+        if is_alarm:
+            sos_device_aid, sos_lat, sos_lon, sos_sent_to = await sync_to_async(
+                self.db_device.send_sos, thread_sensitive=True
+            )()
+            print(
+                f"SOS triggered by device {sos_device_aid}, {sos_lat},"
+                f" {sos_lon} email sent to {sos_sent_to}",
+                flush=True,
+            )
+
     async def _read_line(self):
         try:
             data_bin = b""
             while not data_bin:
                 data_bin = await self.stream.read_until(b"\r\n", 255)
-            if data_bin[:2] == b"\x78\x78" and data_bin[3] == 34:
+            # GPS AND ALARM DATA
+            if data_bin[:2] == b"\x78\x78" and data_bin[3] in (0x22, 0x26):
                 await self._process_data(data_bin)
+            # HEARTBEAT
+            if data_bin[:4] == b"\x78\x78\x0a\x13":
+                serial_number = data_bin[9:11]
+                data_to_send = b"\x05\x13" + serial_number
+                checksum = pack(">H", crc16(data_to_send))
+                self.stream.write(b"\x78\x78" + data_to_send + checksum + b"\r\n")
+
         except Exception as e:
             print(f"Error parsing data: {str(e)}")
             self.stream.close()
