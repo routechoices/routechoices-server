287287592eaed4655b3f23da8fd345934b339829
Raphael Stefanini <raphael@rphlo.com>
2024-03-07 11:07:23 +0200

    Move admin to its own subdomain

--- routechoices/admin/urls.py
+++ routechoices/admin/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
 admin.site.site_header = "Admin"
 admin.site.site_title = "Admin Site"
 admin.site.index_title = "Welcome to the administration site"
+admin.site.site_url = f"//www.{settings.PARENT_HOST}"
 
 urlpatterns = [
     path("", admin.site.urls),
