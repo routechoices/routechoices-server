6f309d709ae63c0a81d95b799a90305cd31a0ade
Raphael Stefanini <raphael@rphlo.com>
2024-03-29 17:59:43 +0200

    Fix missing forgot password link

--- routechoices/admin/urls.py
+++ routechoices/admin/urls.py
@@ -1,13 +1,23 @@
 from django.conf import settings
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
+from django_hosts.resolvers import reverse
 
 admin.site.site_header = "Admin"
 admin.site.site_title = "Admin Site"
 admin.site.index_title = "Welcome to the administration site"
 admin.site.site_url = f"//www.{settings.PARENT_HOST}"
 
+
+def admin_login(request):
+    return redirect(
+        reverse("account_login", host="www") + f"?next=//admin.{settings.PARENT_HOST}"
+    )
+
+
 urlpatterns = [
     path("hijack/", include("hijack.urls")),
+    path("login/", admin_login),
     path("", admin.site.urls),
 ]
