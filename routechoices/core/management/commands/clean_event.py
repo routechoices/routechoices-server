5507c9721e76a53bdcd0b7c52f64f052eb34aaa0
Raphael Stefanini <raphael@rphlo.com>
2024-06-13 08:20:36 +0300

    Refactor ne command into clean code

---
+++ routechoices/core/management/commands/clean_event.py
@@ -0,0 +1,72 @@
+from django.core.management.base import BaseCommand
+
+from routechoices.core.models import Event
+from routechoices.lib.helpers import distance_latlon
+
+
+class Command(BaseCommand):
+    help = "Crop all points of the competitors of an event so that their distances are within a given distance range (in km)"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--event_id", type=str)
+        parser.add_argument("--min_dist", type=float, default=0)
+        parser.add_argument("--max_dist", type=float)
+        parser.add_argument("--force", action="store_true", default=False)
+
+    def handle(self, *args, **options):
+        force = options["force"]
+        min_dist = options["min_dist"]
+        max_dist = options["max_dist"]
+        event = Event.objects.filter(aid=options["event_id"]).first()
+        if not event:
+            self.stderr.write("Event does not exists")
+            return
+        if max_dist and max_dist <= min_dist:
+            self.stderr.write("max_dist must be higher than min_dist")
+            return
+
+        for competitor, start, end in event.iterate_competitors():
+            if not competitor.device:
+                continue
+            prev_point = None
+            total_distance = 0
+            all_locations = competitor.device.locations_series
+            crop_starttime = None
+            for i, point in enumerate(all_locations):
+                if start.timestamp() > point[0]:
+                    continue
+                if point[0] > end.timestamp():
+                    break
+                if prev_point:
+                    total_distance += (
+                        distance_latlon(
+                            {"lat": point[1], "lon": point[2]},
+                            {"lat": prev_point[1], "lon": prev_point[2]},
+                        )
+                        / 1000
+                    )
+                if max_dist and total_distance > max_dist:
+                    crop_starttime = point[0]
+                    break
+                prev_point = point
+            if total_distance < min_dist:
+                if force:
+                    competitor.device = None
+                    competitor.save()
+                self.stdout.write(f"Removing all points for {competitor}")
+                continue
+            if crop_starttime:
+                cropped_locations = [
+                    point
+                    for point in all_locations
+                    if (point[0] < crop_starttime or point[0] > end.timestamp())
+                ]
+                nb_points_cropped = len(all_locations) - len(cropped_locations)
+                if force:
+                    competitor.device.locations_series = cropped_locations
+                    competitor.device.save()
+                self.stdout.write(
+                    f"Cropping {nb_points_cropped} locations for {competitor}"
+                )
+        if not force:
+            self.stderr.write("This was a dry run....")
