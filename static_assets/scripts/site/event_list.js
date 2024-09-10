ab7bc0853f7bf8ece07d70c0b07ffa952ca9a6a1
Raphael Stefanini <raphael@rphlo.com>
2024-09-10 17:18:29 +0300

    Lazy load background images in event list

--- static_assets/scripts/site/event_list.js
+++ static_assets/scripts/site/event_list.js
@@ -9,4 +9,40 @@
   tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
+
+  if ("IntersectionObserver" in window) {
+    document.addEventListener("DOMContentLoaded", function () {
+      function handleIntersection(entries) {
+        entries.map((entry) => {
+          if (entry.isIntersecting && entry.target.dataset.bgImg) {
+            // Item has crossed our observation
+            // threshold - load src from data-src
+            entry.target.style.background =
+              "linear-gradient(to right, var(--rc-color-from-trans), var(--rc-color-to-trans)), url('" +
+              entry.target.dataset.bgImg +
+              "')";
+            // Job done for this item - no need to watch it!
+            observer.unobserve(entry.target);
+          }
+        });
+      }
+
+      const events = document.querySelectorAll(".event-card");
+      const observer = new IntersectionObserver(handleIntersection, {
+        rootMargin: "100px",
+      });
+      events.forEach((event) => observer.observe(event));
+    });
+  } else {
+    // No interaction support? Load all background images automatically
+    const events = document.querySelectorAll(".event-card");
+    events.forEach((event) => {
+      if (event.dataset.bgImg) {
+        event.style.background =
+          "linear-gradient(to right, var(--rc-color-from-trans), var(--rc-color-to-trans)), url('" +
+          header.dataset.bgImg +
+          "')";
+      }
+    });
+  }
 })();
