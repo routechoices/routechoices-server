2228f93ac2bdfdcddfd8b0185530cb4e9e2d10de
Raphael Stefanini <raphael@rphlo.com>
2024-10-10 14:56:02 +0300

    Add e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
+  chromeWebSecurity: true,
 
   e2e: {
     // We've imported your old cypress plugins here.
