530f000ead7c4284d1984270b60547118e47aae5
Raphael Stefanini <raphael@rphlo.com>
2024-10-10 15:06:23 +0300

    Add e2e tests

--- cypress/e2e/future-events.cy.js
+++ cypress/e2e/future-events.cy.js
@@ -8,18 +8,20 @@ context("Events in future", () => {
   });
 
   it("Future events are not listed", function () {
-    cy.forceVisit("/halden-sk/");
+    cy.forceVisit("https://halden-sk.routechoices.dev/");
     cy.contains("My Future").should("not.exist");
   });
 
   it("Can not see Event as in future", function () {
-    cy.forceVisit("/halden-sk/future-default");
+    cy.forceVisit("https://halden-sk.routechoices.dev/future-default");
     cy.contains("Event has not yet started.");
     cy.contains("Starting in").should("be.visible");
   });
 
   it("Can not register or upload to Event as registration not open", function () {
-    cy.forceVisit("/halden-sk/future-default/contribute");
+    cy.forceVisit(
+      "https://halden-sk.routechoices.dev/future-default/contribute"
+    );
     cy.get("#registration-form").should("not.exist");
     cy.get("#upload-form").should("not.exist");
   });
@@ -30,7 +32,9 @@ context("Events in future", () => {
   });
 
   it("Can register to an Event if open registration", function () {
-    cy.forceVisit("/halden-sk/future-open-registration/contribute");
+    cy.forceVisit(
+      "https://halden-sk.routechoices.dev/future-open-registration/contribute"
+    );
     cy.contains("Enter yourself");
     cy.get("#id_name").type("Thierry Gueorgiou");
     cy.get("#id_short_name").type("🇫🇷 T.Gueorgiou");
@@ -40,14 +44,16 @@ context("Events in future", () => {
   });
 
   it("Can not upload to an Event even if upload allowed since it is not yet started", function () {
-    cy.forceVisit("/halden-sk/future-upload-allowed/contribute");
+    cy.forceVisit(
+      "https://halden-sk.routechoices.dev/future-upload-allowed/contribute"
+    );
     cy.get("#registration-form").should("not.exist");
     cy.get("#upload-form").should("not.exist");
   });
 
   it("Can only add competitor and not upload route to an Event if everything allowed even since it is not yet started", function () {
     cy.forceVisit(
-      "/halden-sk/future-open-registration-upload-allowed/contribute"
+      "https://halden-sk.routechoices.dev/future-open-registration-upload-allowed/contribute"
     );
     cy.get("#registration-form").should("exist");
     cy.get("#upload-form").should("not.exist");
