d45cbbaad69eda32c9c8f11eb660671f98aa2dca
Raphael Stefanini <raphael@rphlo.com>
2024-10-10 14:59:29 +0300

    Add e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: true,
-
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -11,11 +10,13 @@ module.exports = defineConfig({
     },
     baseUrl: "https://www.routechoices.dev",
   },
-
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
     },
+  },
+  hosts: {
+    "*.routechoices.dev": "127.0.0.1",
   },
 });
